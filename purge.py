e4f9065399cbbb4ed8a459fb7054441d573a7815
Matheus Sobral <matheusalucard.js@gmail.com>
2024-06-20 15:10:30 -0300

    Add files via upload

---
+++ purge.py
@@ -0,0 +1,177 @@
+import discord
+from time import sleep
+from pystyle import Center, Colors, Colorate
+import asyncio
+from colorama import init, Fore
+import requests, os, platform, time
+
+os.system('mode 130, 50')
+os.system('cls')
+sleep(0.3)
+
+print(Colorate.Color(Colors.white,
+"""                                                                   
+                             ╔═══════════════════════════════════════╗ 
+                             ║          Carregando sistema . . .     ║
+                             ╚═══════════════════════════════════════╝ 
+\n
+
+    """))
+    
+os.system('title Carregando'),
+sleep(0.1)
+os.system('title Carregando .'),
+sleep(0.2)
+os.system('title Carregando . .'),
+sleep(0.3)
+os.system('title Carregando . . .'),
+sleep(0.4)
+os.system('title Carregando . . . .'),
+sleep(0.5)
+os.system('title Carregando . . . . .'),
+sleep(0.6)
+    
+os.system('cls')    
+os.system('title S'),
+sleep(0.02)
+os.system('title Sa'),
+sleep(0.03)
+os.system('title Sat'),
+sleep(0.04)
+os.system('title Satu'),
+sleep(0.05)
+os.system('title Satus'),
+sleep(0.06)
+os.system('title Satush'),
+sleep(0.07)
+os.system('title Satushi')
+sleep(0.08)
+os.system('title Satushi ')
+sleep (0.09)
+os.system('title Satusshi - Purge')
+sleep (0.10)
+
+print(Colorate.Color(Colors.cyan,
+"""        
+            ███████╗ █████╗ ████████╗██╗   ██╗███████╗██╗  ██╗██╗
+            ██╔════╝██╔══██╗╚══██╔══╝██║   ██║██╔════╝██║  ██║██║
+            ███████╗███████║   ██║   ██║   ██║███████╗███████║██║
+            ╚════██║██╔══██║   ██║   ██║   ██║╚════██║██╔══██║██║
+            ███████║██║  ██║   ██║   ╚██████╔╝███████║██║  ██║██║
+            ╚══════╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝                                                           
+\n
+
+    """))
+token = input('Ready? Pronto?: ')
+
+os.system("cls")
+
+os.system('cls')
+os.system('title S'),
+sleep(0.02)
+os.system('title Sa'),
+sleep(0.03)
+os.system('title Sat'),
+sleep(0.04)
+os.system('title Satu'),
+sleep(0.05)
+os.system('title Satus'),
+sleep(0.06)
+os.system('title Satush'),
+sleep(0.07)
+os.system('title Satushi')
+sleep(0.08)
+os.system('title Satushi ')
+sleep (0.09)
+os.system('title Satushi - Palavra Chave')
+
+print(Colorate.Color(Colors.cyan,
+"""
+          
+
+
+  ██████  ▄▄▄     ▄▄▄█████▓ █    ██   ██████  ██░ ██  ██▓
+▒██    ▒ ▒████▄   ▓  ██▒ ▓▒ ██  ▓██▒▒██    ▒ ▓██░ ██▒▓██▒
+░ ▓██▄   ▒██  ▀█▄ ▒ ▓██░ ▒░▓██  ▒██░░ ▓██▄   ▒██▀▀██░▒██▒
+  ▒   ██▒░██▄▄▄▄██░ ▓██▓ ░ ▓▓█  ░██░  ▒   ██▒░▓█ ░██ ░██░
+▒██████▒▒ ▓█   ▓██▒ ▒██▒ ░ ▒▒█████▓ ▒██████▒▒░▓█▒░██▓░██░
+▒ ▒▓▒ ▒ ░ ▒▒   ▓▒█░ ▒ ░░   ░▒▓▒ ▒ ▒ ▒ ▒▓▒ ▒ ░ ▒ ░░▒░▒░▓  
+░ ░▒  ░ ░  ▒   ▒▒ ░   ░    ░░▒░ ░ ░ ░ ░▒  ░ ░ ▒ ░▒░ ░ ▒ ░
+░  ░  ░    ░   ▒    ░       ░░░ ░ ░ ░  ░  ░   ░  ░░ ░ ▒ ░
+      ░        ░  ░           ░           ░   ░  ░  ░ ░  
+                                                         
+
+                                                                                           
+  
+     """))
+syntax = input('Keyword for clean / Palavra chave para executar a limpeza: ')
+os.system("cls")
+
+os.system('cls')
+os.system('title S'),
+sleep(0.02)
+os.system('title Sa'),
+sleep(0.03)
+os.system('title Sat'),
+sleep(0.04)
+os.system('title Satu'),
+sleep(0.05)
+os.system('title Satus'),
+sleep(0.06)
+os.system('title Satush'),
+sleep(0.07)
+os.system('title Satushi')
+sleep(0.08)
+os.system('title Satushi ')
+sleep (0.09)
+os.system('title Satushi - Pronto para ação')
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(Colorate.Color(Colors.cyan,
+        f"""
+            
+
+   ▄████████    ▄████████     ███     ███    █▄     ▄████████    ▄█    █▄     ▄█  
+  ███    ███   ███    ███ ▀█████████▄ ███    ███   ███    ███   ███    ███   ███  
+  ███    █▀    ███    ███    ▀███▀▀██ ███    ███   ███    █▀    ███    ███   ███▌ 
+  ███          ███    ███     ███   ▀ ███    ███   ███         ▄███▄▄▄▄███▄▄ ███▌ 
+▀███████████ ▀███████████     ███     ███    ███ ▀███████████ ▀▀███▀▀▀▀███▀  ███▌ 
+         ███   ███    ███     ███     ███    ███          ███   ███    ███   ███  
+   ▄█    ███   ███    ███     ███     ███    ███    ▄█    ███   ███    ███   ███  
+ ▄████████▀    ███    █▀     ▄████▀   ████████▀   ▄████████▀    ███    █▀    █▀   
+                                                                                  
+
+
+
+        """))
+        print('                   ╔═══════════════════════════════════════╗ ')
+        print('                              Usuário:  {0}        '.format(self.user))
+        print(f'                              Prefixo:  {syntax}           ')
+        print("""                              Mensagens sendo apagadas . .    
+                   ╚═══════════════════════════════════════╝ """)
+    async def on_message(self, message):
+        if(message.author!=self.user):
+            return
+        channels=[]
+        if(message.content=="purge_server"):
+            channels=message.channel.guild.channels
+        elif(message.content==syntax):  
+            channels.append(message.channel)
+        else:
+            return
+        for channel in channels:
+            print(channel)
+            try:
+                async for mss in channel.history(limit=None):
+                    if(mss.author==self.user):
+                        print(f"{Fore.RED}Deletado:{Fore.WHITE} {mss.content}")
+                        try:
+                            await mss.delete()
+                        except:
+                            print(f"{Fore.RED}Sou incapaz de deletar:{Fore.RED} {mss.content}\n")
+            except:
+                print(f"{Fore.RED}Não consigo ler o histórico.\n")
+
+client=MyClient(heartbeat_timeout=86400, guild_subscriptions=False)
+client.run('token')
